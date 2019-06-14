1148c99218a173d65326aa0debf5425035371f2e
gojimmypi <gojimmypi@gmail.com>
2019-06-14 13:53:18 -0700

    cleanup; re-add missing comments

--- passthru/OpenSource/passthru.v
+++ passthru/OpenSource/passthru.v
@@ -1,8 +1,7 @@
 // File ulx3s_v20_passthru_wifi.vhd translated with vhd2vl v3.0 VHDL to Verilog RTL translator
-//
 // this is a freshly converted 6a044e8 version from March 2
 // see https://github.com/emard/ulx3s-passthru/blob/71ce18953f84ea8ee07bb42d42ddc5a2673623c3/rtl/ulx3s_v20_passthru_wifi.vhd#L1
-//
+// 
 // vhd2vl settings:
 //  * Verilog Module Declaration Style: 2001
 
@@ -30,21 +29,29 @@
 // no timescale needed
 
 module ulx3s_passthru_wifi(
-input wire clk_25mhz,
+input wire clk_25mhz, // -- main clock input from 25MHz clock source must be lowercase
+// UART0 (FTDI USB slave serial)
 output wire ftdi_rxd,
 input wire ftdi_txd,
+
+// FTDI additional signaling
 inout wire ftdi_ndtr,
-inout wire ftdi_ndsr,
+// inout wire ftdi_ndsr, // ERROR: IO 'ftdi_ndsr' is unconstrained in LPF
 inout wire ftdi_nrts,
 inout wire ftdi_txden,
+
+// UART1 (WiFi serial)
 output wire wifi_rxd,
 input wire wifi_txd,
 inout wire wifi_en,
+
+// WiFi additional signaling
 inout wire wifi_gpio0,
-inout wire wifi_gpio2,
+// inout wire wifi_gpio2, // ERROR: IO 'wifi_gpio2' is unconstrained in LPF
 inout wire wifi_gpio5,
 inout wire wifi_gpio16,
 inout wire wifi_gpio17,
+// Onboard blinky
 output wire [7:0] led,
 input wire [6:0] btn,
 input wire [1:4] sw,
@@ -53,19 +60,28 @@ output wire oled_clk,
 output wire oled_mosi,
 output wire oled_dc,
 output wire oled_resn,
+
+// GPIO (some are shared with wifi and adc)
 inout wire [27:0] gp,
 inout wire [27:0] gn,
+// SHUTDOWN: logic '1' here will shutdown power on PCB >= v1.7.5
 output wire shutdown,
+
+// Audio jack 3.5mm
 inout wire [3:0] audio_l,
 inout wire [3:0] audio_r,
 inout wire [3:0] audio_v,
+// Flash ROM (SPI0)
 output wire flash_holdn,
 output wire flash_wpn,
+
+// SD card (SPI1)
 inout wire [3:0] sd_d,
 input wire sd_cmd,
 input wire sd_clk,
 input wire sd_cdn,
 input wire sd_wp,
+
 output wire user_programn
 );
 
@@ -95,9 +111,12 @@ parameter [31:0] C_dummy_constant=0;
 // setting this low will skip to next multiboot image
 
 
+  assign shutdown = 0'b0;
 
-wire [1:0] S_prog_in; reg [1:0] R_prog_in; wire [1:0] S_prog_out;
-reg [7:0] R_spi_miso;
+wire [1:0] S_prog_in;
+reg  [1:0] R_prog_in; 
+wire [1:0] S_prog_out;
+reg  [7:0] R_spi_miso;
 wire S_oled_csn;
 parameter C_prog_release_timeout = 17;  // default 17 2^n * 25MHz timeout for initialization phase
 reg [C_prog_release_timeout:0] R_prog_release = 1'b1;  // timeout that holds lines for reliable entering programming mode
@@ -116,8 +135,10 @@ reg [7:0] R_progn = 1'b0;
   assign S_prog_in[1] = ftdi_ndtr;
   assign S_prog_in[0] = ftdi_nrts;
   assign S_prog_out = S_prog_in == 2'b10 ? 2'b01 : S_prog_in == 2'b01 ? 2'b10 : 2'b11;
+
   assign wifi_en = S_prog_out[1];
   assign wifi_gpio0 = S_prog_out[0] & btn[0];
+
   // holding BTN0 will hold gpio0 LOW, signal for ESP32 to take control
   //sd_d(0) <= '0' when wifi_gpio0 = '0' else 'Z'; -- gpio2 together with gpio0 to 0
   //sd_d(2) <= '0' when wifi_gpio0 = '0' else 'Z'; -- wifi gpio12
@@ -144,6 +165,7 @@ reg [7:0] R_progn = 1'b0;
   // led(7 downto 0) <= S_oled_csn & R_spi_miso(0) & sd_clk & sd_d(2) & sd_d(3) & sd_cmd & sd_d(0) & sd_d(1); -- beautiful but makes core unreliable
   assign led[7] = wifi_gpio5;
   // for boards without D22 soldered
+  // ESP32 programming start: blinks too short to be visible
   assign led[6] = S_prog_out[1];
   // green LED indicates ESP32 disabled
   assign led[5] =  ~R_prog_release[(C_prog_release_timeout)];
