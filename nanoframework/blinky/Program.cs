f51ce1027fb8d1cc3be55328c3a7c473f0e4071b
gojimmypi <gojimmypi@gmail.com>
2020-01-23 06:10:11 -0800

    initial blinky (that is actually just hello world), no blink

---
+++ nanoframework/blinky/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+ 
+
+namespace blinky
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello world!");
+            int a = 1;
+            Console.WriteLine("Hello world! " + a.ToString());
+
+            Thread.Sleep(Timeout.Infinite);
+
+            // Browse our samples repository: https://github.com/nanoframework/samples
+            // Check our documentation online: https://docs.nanoframework.net/
+            // Join our lively Discord community: https://discord.gg/gCyBu8T
+        }
+    }
+}
